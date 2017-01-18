af95d970507e1ce848a0e2c44100c1742557b50d
vincenzo vignieri <vincenzovignieri@vincenzos-MacBook-Air.local>
2017-01-18 18:16:21 +0100

    Initial Commit

---
+++ pokidex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokidex3
+//
+//  Created by vincenzo vignieri on 18/01/2017.
+//  Copyright © 2017 vincenzo vignieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
